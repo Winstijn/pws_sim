b664c9a3c74b50b60e87bee3d8b0cd3303c8881e
Winstijn Smit <winstijn@hotmail.nl>
2019-09-06 11:14:23 +0200

    Examples for Fyor's AI code

--- public/js/ai.js
+++ public/js/ai.js
@@ -6,6 +6,48 @@
 //  Copyright © 2019 Kaziaat B.V. All rights reserved.
 //
 
+// Example for Fyor.
+class SelfDrivingAI {
+
+    constructor(settings) {
+
+        if (settings.random) {
+            this.generateRandomAI()
+        }
+
+        this.canvas = __SIMULATOR_CANVAS
+        this.id = "fjkabfjsfuwhr209u4"
+        this.values = [0, 30,4, 5,5,]
+        this.fitness = 0
+
+        // 
+    }
+
+    // called everyframe
+    draw() {
+        if 
+    }
+
+    generateRandomAI(){
+
+    }
+
+}
+
+$(document).ready( () => {
+    var aiCount = 100
+    var ais = []
+    for (let i = 0; i < aiCount; i++) {
+        ais.push( new SelfDrivingAI({ random: true, val })  )
+        
+    }
+    
+
+})
+
+
+
+
 // Ik wil dat je luisterd naar een document.ready, ofwel
 // $(document),ready( () => { bla code} )
 // Als dit niet begrijpt, ga je maar wat documentatie lezen :)
